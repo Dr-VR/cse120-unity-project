f9239dc6ee781c90bac29ca12671b60e2572bd9f
sfrutos <44537981+sfrutos@users.noreply.github.com>
2020-04-24 10:10:37 -0700

    working scene with eye data print to console
    Using TobiiXR able to print eye gaze, completed scene, and highlight at gaze feature implemented. TODO: eye gaze is normalized and data needs to be saved.

---
+++ 3D_VR/Library/Assets/Main Scene/Scripts/move.cs
@@ -0,0 +1,69 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXValue = Input.GetAxis("Mouse X");
+        float mouseYValue = Input.GetAxis("Mouse Y");
+
+        if (mouseXValue != 0)
+        {
+            print("Mouse X movement: " + mouseXValue);
+        }
+        if (mouseYValue != 0)
+        {
+            print("Mouse Y movement: " + mouseYValue);
+        }
+    }
+}
+
+using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+    public GameObject particle;
+    void Update()
+    {
+        float mouseXValue = Input.GetAxis("Mouse X");
+        float mouseYValue = Input.GetAxis("Mouse Y");
+
+            if (mouseXValue != 0)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray))
+                    print("Mouse X movement: " + mouseXValue);
+            }
+            if (mouseYValue != 0)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray))
+                    print("Mouse Y movement: " + mouseYValue);
+        }
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Transform eyeDest;
+
+    void Update()
+    {
+        transform.LookAt(eyeDest);
+    }
+}
